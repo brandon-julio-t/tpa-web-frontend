ad596d5b79ca62700837fb3a8f98d965c9e9c297
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-01-18 23:31:19 +0700

    top up feature

---
+++ src/app/pages/top-up/top-up.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Apollo, gql } from 'apollo-angular';
+
+@Component({
+  selector: 'app-top-up',
+  templateUrl: './top-up.component.html',
+  styleUrls: ['./top-up.component.scss'],
+})
+export class TopUpComponent implements OnInit {
+  topUpForm: FormGroup;
+  isLoading = false;
+
+  constructor(private apollo: Apollo, private fb: FormBuilder) {
+    this.topUpForm = fb.group({
+      code: fb.control('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    this.isLoading = true;
+
+    this.apollo
+      .mutate<{ redeemWallet: boolean }>({
+        mutation: gql`
+          mutation redeemWallet($code: String!) {
+            redeemWallet(code: $code)
+          }
+        `,
+        variables: this.topUpForm.value,
+      })
+      .subscribe((resp) => {
+        if (resp.data?.redeemWallet) {
+          alert('Success');
+          this.isLoading = false;
+        }
+      });
+  }
+}
