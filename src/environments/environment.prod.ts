b3b39bef42daf8db549c31fc5922cb81462ad8c7
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-01-21 03:01:04 +0700

    fix secure websocket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://tpa-web-backend-br20-2.herokuapp.com/graphql',
-  wsUrl: 'ws://tpa-web-backend-br20-2.herokuapp.com/graphql',
+  wsUrl: 'wss://tpa-web-backend-br20-2.herokuapp.com/graphql',
 };
